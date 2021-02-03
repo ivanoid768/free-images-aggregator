2fa8582933769cc5a37d31082f013a949ab6e47d
Ivan Pkh <ivanoid768@users.noreply.github.com>
2021-02-03 18:15:52 +0500

    Save image data to db.

--- workers/download_from_source.js
+++ workers/download_from_source.js
@@ -1,14 +1,14 @@
 import { worker } from 'workerpool';
 import fetch from 'node-fetch';
-import {} from 'pg';
+import pkg from 'pg';
 
 import { CONFIG, SOURCE } from '../config.js';
 
-// a deliberately inefficient implementation of the fibonacci sequence
-function fibonacci(n) {
-    if (n < 2) return n;
-    return fibonacci(n - 2) + fibonacci(n - 1);
-}
+const { Client } = pkg;
+
+const client = new Client({
+    connectionString: CONFIG.PG_DB_CONNECTION_URI,
+})
 
 async function GetImageDataFromPixabay({ page }) {
     let url = `https://pixabay.com/api/?key=${CONFIG.PIXABAY_API_KEY}&q=${CONFIG.CATEGORY}&page=${page}&per_page=200`
@@ -16,7 +16,7 @@ async function GetImageDataFromPixabay({ page }) {
     let res = await fetch(url)
     console.log(res.status, res.statusText);
 
-    if(res.status == 502){
+    if (res.status == 502) {
         console.log('res.text: ', await res.text());
 
         return {
@@ -41,7 +41,7 @@ async function GetImageDataFromPixabay({ page }) {
 
     let total = image_data.totalHits;
 
-    console.log('totalHits: ' , image_data.totalHits, 'total', image_data.total);
+    console.log('totalHits: ', image_data.totalHits, 'total', image_data.total);
 
     return {
         images,
@@ -76,8 +76,49 @@ function GetImageFromSource({ source, page }) {
     }
 }
 
-function theWorker({ source, page }) {
-    return GetImageFromSource({ source, page })
+async function saveImagesToDB(images) {
+    await client.connect()
+
+    // let insertText = `INSERT INTO distributors (did, dname)
+    // VALUES (5, 'Gizmo Transglobal'), (6, 'Associated Computing, Inc')
+    // ON CONFLICT (did) DO UPDATE SET dname = EXCLUDED.dname;`
+
+    // INSERT INTO films (code, title, did, date_prod, kind) VALUES
+    // ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'),
+    // ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');
+
+    // source: SOURCE.PIXABAY,
+    // id: hit.id,
+    // imageURL: hit.largeImageURL,
+    // searchText: `${hit.tags} ${hit.pageURL}`,
+    // userId: hit.user_id,
+    // username: hit.user,
+    // pageURL: hit.pageURL,
+
+    let valuesText = images.map(image=>{
+        return `(${image.id},'${image.source}','${image.imageURL}','${image.searchText}',${image.userId},'${image.username}','${image.pageURL}')`
+    }).join(',')
+
+    let updateValuesText = `(EXCLUDED.source_name, EXCLUDED.image_url, EXCLUDED.search_text, EXCLUDED.user_id, EXCLUDED.username, EXCLUDED.page_url)`
+
+    let insertText = `
+        INSERT INTO images (image_id, source_name, image_url, search_text, user_id, username, page_url)
+        VALUES ${valuesText}
+        ON CONFLICT (image_id) DO UPDATE SET (source_name, image_url, search_text, user_id, username, page_url) = ${updateValuesText};
+    `
+
+    console.log('insertText: ', insertText);
+
+    const res = await client.query(insertText)
+    console.log(res.command, res)
+    await client.end()
+}
+
+async function theWorker({ source, page }) {
+    let imagesResp = await GetImageFromSource({ source, page })
+    await saveImagesToDB(imagesResp.images)
+
+    return imagesResp;
 }
 
 // create a worker and register public functions
